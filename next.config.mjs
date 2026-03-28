21d172ecffb27684c690d5d38f7d52b494362abf
Arsenides <d332zms@hotmail.com>
2026-03-28 16:44:30 +0800

    更新图片缩放功能

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,13 @@ import nextra from 'nextra'
 
 // Set up Nextra with its configuration
 const withNextra = nextra({
-    search: false // 禁用搜索
     // ... Add Nextra-specific options here
 })
 
 // Export the final Next.js config with Nextra included
 export default withNextra({
+    images: {
+        unoptimized: true // 禁用图片优化
+    }
     // ... Add regular Next.js options here
 })
