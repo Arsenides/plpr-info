d135a2d6fcb37db5970ec0f74c5f7d84a46c99de
Arsenides <d332zms@hotmail.com>
2026-09-22 21:41:34 +0800

    月下篇05-06

---
+++ app/other/_meta.js
@@ -0,0 +1,20 @@
+export default {
+    index : {
+        title: '总览'
+    },
+    live : {
+        title: '现场演唱会'
+    },
+    goods : {
+        title: '谷子情报'
+    },
+    misc : {
+        title: '杂项'
+    },
+    'character-interview' : {
+        title: '角色访谈'
+    },
+    'cast-interview' : {
+        title: '声优采访'
+    }
+}
